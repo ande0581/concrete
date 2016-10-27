b68ba1c9e24bf9f046921095415f4e36f8242239
janderson <jeffrey.d.anderson@gmail.com>
2016-10-26 21:50:00 -0500

    updated db test data script to include more db entries

--- concrete_project/populate_db.py
+++ concrete_project/populate_db.py
@@ -3,6 +3,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'concrete_project.settings')
 
 import django
 django.setup()
+from django.utils import timezone
 from address.models import Address
 from bid.models import Bid
 from customer.models import Customer
@@ -12,11 +13,7 @@ from service.models import Service
 
 
 def populate():
-    # First, we will create lists of dictionaries containing the pages
-    # we want to add into each category.
-    # Then we will create a dictionary of dictionaries for our categories.
-    # This might seem a little bit confusing, but it allows us to iterate
-    # through each data structure, and add the data to our models.
+    print("Current Time with TZ:", timezone.now(),)
 
     customer_records = [
         {'name': 'Jeff Johnson', 'email': 'jeff@domain.com', 'telephone': '7635551000', 'pk': 1},
@@ -24,15 +21,15 @@ def populate():
     ]
 
     address_records = [
-        {'street': '123 washington ave', 'city': 'duluth', 'state': 'mn', 'zip': '55111', 'customer_id': 1},
-        {'street': '456 delaware lane', 'city': 'clearwater', 'state': 'mn', 'zip': '55111', 'customer_id': 2}
+        {'street': '123 washington ave', 'city': 'duluth', 'state': 'mn', 'zip': '55111', 'customer_id': 1, 'pk': 1},
+        {'street': '456 delaware lane', 'city': 'clearwater', 'state': 'mn', 'zip': '55111', 'customer_id': 2, 'pk': 2}
     ]
 
     journal_records = [
-        {'body': 'Call customer to follow up on scheduling', 'timestamp': '2016-02-29 10:42:52.509000',
-         'customer_id': 1},
-        {'body': 'Call customer setup time to seal driveway', 'timestamp': '2016-03-09 11:21:38.934000',
-         'customer_id': 2}
+        {'body': 'Call customer to follow up on scheduling', 'timestamp': timezone.now(),
+         'customer_id': 1, 'pk': 1},
+        {'body': 'Call customer setup time to seal driveway', 'timestamp': timezone.now(),
+         'customer_id': 2, 'pk': 2}
     ]
 
     service_records = [
@@ -42,17 +39,17 @@ def populate():
 
     bid_records = [
         {'description': 'pour new driveway and apron', 'notes': 'no smoking on property', 'customer_id': 1,
-         'address_id': 1},
+         'address_id': 1, 'timestamp': timezone.now(), 'pk': 1},
         {'description': 'garage floor 40x60', 'notes': 'use hurricane straps', 'customer_id': 2,
-         'address_id': 2},
+         'address_id': 2, 'timestamp': timezone.now(), 'pk': 2}
     ]
 
-    biditem_records = [
-        {'bid_id': 1, 'service_id': 1, 'description': '.5 inch rebar', 'cost': 5.60, 'quantity': 15, 'total': 84},
-        {'bid_id': 1, 'service_id': 2, 'description': 'standard gray concrete', 'cost': 3.25, 'quantity': 200,
+    bid_item_records = [
+        {'bid_id': 1, 'description': '.5 inch rebar', 'cost': 5.60, 'quantity': 15, 'total': 84},
+        {'bid_id': 1, 'description': 'standard gray concrete', 'cost': 3.25, 'quantity': 200,
          'total': 650},
-        {'bid_id': 2, 'service_id': 1, 'description': '.5 inch rebar', 'cost': 5.60, 'quantity': 10, 'total': 56},
-        {'bid_id': 2, 'service_id': 2, 'description': 'standard gray concrete', 'cost': 3.25, 'quantity': 100,
+        {'bid_id': 2, 'description': '.5 inch rebar', 'cost': 5.60, 'quantity': 10, 'total': 56},
+        {'bid_id': 2, 'description': 'standard gray concrete', 'cost': 3.25, 'quantity': 100,
          'total': 325}
     ]
 
@@ -62,16 +59,28 @@ def populate():
     for record in address_records:
         add_address(record)
 
+    for record in journal_records:
+        add_journal(record)
+
+    for record in service_records:
+        add_service(record)
+
+    for record in bid_records:
+        add_bid(record)
+
+    for record in bid_item_records:
+        add_bid_item(record)
+
 
 def add_customer(cust_entry):
-    print(cust_entry)
+    print("Customer:", cust_entry)
     cus_obj = Customer.objects.get_or_create(**cust_entry)[0]
     cus_obj.save()
     return cus_obj
 
 
 def add_address(addr_entry):
-    print(addr_entry)
+    print("Address:", addr_entry)
     cus_obj = Customer.objects.get(pk=addr_entry['customer_id'])
     addr_entry['customer_id'] = cus_obj
     addr_obj = Address.objects.get_or_create(**addr_entry)[0]
@@ -79,49 +88,40 @@ def add_address(addr_entry):
     return addr_obj
 
 
-"""
-    def add_page(cat, title, url, views=0):
-        p = Page.objects.get_or_create(category=cat, title=title)[0]
-        p.url = url
-        p.views = views
-        p.save()
-        return p
-"""
+def add_journal(journal_entry):
+    print("Journal:", journal_entry)
+    cus_obj = Customer.objects.get(pk=journal_entry['customer_id'])
+    journal_entry['customer_id'] = cus_obj
+    journal_obj = Journal.objects.get_or_create(**journal_entry)[0]
+    journal_obj.save()
+    return journal_obj
 
-"""
-41
-42     # If you want to add more catergories or pages,
-43     # add them to the dictionaries above.
-44
-45     # The code below goes through the cats dictionary, then adds each category,
-46     # and then adds all the associated pages for that category.
-47     # if you are using Python 2.x then use cats.iteritems() see
-48     # http://docs.quantifiedcode.com/python-anti-patterns/readability/
-49     # for more information about how to iterate over a dictionary properly.
-50
-51     for cat, cat_data in cats.items():
-52         c = add_cat(cat)
-53         for p in cat_data["pages"]:
-54             add_page(c, p["title"], p["url"])
-55
-56     # Print out the categories we have added.
-57     for c in Category.objects.all():
-58         for p in Page.objects.filter(category=c):
-59             print("- {0} - {1}".format(str(c), str(p)))
-60
-61 def add_page(cat, title, url, views=0):
-62     p = Page.objects.get_or_create(category=cat, title=title)[0]
-63     p.url=url
-64     p.views=views
-65     p.save()
-66     return p
-67
-68 def add_cat(name):
-69     c = Category.objects.get_or_create(name=name)[0]
-70     c.save()
-71     return c
-72
-"""
+
+def add_service(service_entry):
+    print("Service:", service_entry)
+    service_obj = Service.objects.get_or_create(**service_entry)[0]
+    service_obj.save()
+    return service_obj
+
+
+def add_bid(bid_entry):
+    print("Bid:", bid_entry)
+    addr_obj = Address.objects.get(pk=bid_entry['address_id'])
+    cus_obj = Customer.objects.get(pk=bid_entry['customer_id'])
+    bid_entry['address_id'] = addr_obj
+    bid_entry['customer_id'] = cus_obj
+    bid_obj = Bid.objects.get_or_create(**bid_entry)[0]
+    bid_obj.save()
+    return bid_obj
+
+
+def add_bid_item(bid_item_entry):
+    print("Bid Item:", bid_item_entry)
+    bid_obj = Bid.objects.get(pk=bid_item_entry['bid_id'])
+    bid_item_entry['bid_id'] = bid_obj
+    bid_item_obj = BidItem.objects.get_or_create(**bid_item_entry)[0]
+    bid_item_obj.save()
+    return bid_item_obj
 
 
 # Start execution here!
