6f407e50142af31e90dce3cf74d4982bf4ddba07
ande0581 <jeffrey.d.anderson@gmail.com>
2017-01-01 11:44:25 -0600

    basic class based create view working

--- src/address/urls.py
+++ src/address/urls.py
@@ -1,16 +1,10 @@
 from django.conf.urls import url
-#from django.views.generic.base import TemplateView
-
-from address.views import address_model_create_view, AddressDetail, AddressList
+from address.views import AddressDetail, AddressList, AddressCreate
 
 
 app_name = 'address_app'
 urlpatterns = [
-    #url(r'^$', views.customer_model_list_view, name='list'),
     url(r'^$', AddressList.as_view(), name='address_list'),
     url(r'^(?P<pk>\d+)/$', AddressDetail.as_view(), name='address_detail'),
-    url(r'^create/(?P<customer_id>\d+)/$', address_model_create_view, name='create'),
-    #url(r'^(?P<pk>\d+)/$', views.customer_model_detail_view, name='detail'),
-    #url(r'^(?P<pk>\d+)/delete/$', views.customer_model_delete_view, name='delete'),
-    #url(r'^(?P<pk>\d+)/edit/$', views.customer_model_update_view, name='update'),
+    url(r'^create/(?P<customer_id>\d+)/$', AddressCreate.as_view(), name='address_create'),
 ]
--- src/address/views.py
+++ src/address/views.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 
 from address.models import Address
@@ -12,41 +13,61 @@ from customer.models import Customer
 from address.forms import AddressForm
 
 
+# class ExampleCreate(CreateView):
+#     model = Example
+#     fields = ['street', 'state', 'city', 'zip', 'customer_id']
+#
+#     # optional template, or it defaults to example_form.html
+#     template_name = "xxx.html"
+#
+#     # optional form, when you use this, you omit the fields = []
+#     form_class = ExampleForm
+#
+#     # to change a value after the form is posted
+#     def form_valid(self, form):
+#         #print('form.instance:', form.instance)
+#         #print('kwargs:', self.kwargs)
+#         form.instance.customer_id = Customer.objects.get(pk=self.kwargs['customer_id'])
+#         #print('form.instance.customer_id:', form.instance.customer_id)
+#         return super(ExampleCreate, self).form_valid(form)
+
+
+# class ExampleDetail(DetailView):
+#     model = Example
+#
+#     # this is optional to see the context
+#     def get_context_data(self, **kwargs):
+#         context = super(AddressDetail, self).get_context_data(**kwargs)
+#         print(context)
+#         return context
+
+
+# class ExampleList(ListView):
+#     model = Example
+#
+#     # this is optional way to change the queryset
+#     def get_queryset(self, *args, **kwargs):
+#         qs = super(AddressList, self).get_queryset(*args, **kwargs).order_by('-street')
+#         #qs = super(AddressList, self).get_queryset(*args, **kwargs).filter(title__startswith='670')
+#         print(qs)
+#         print(qs.first())
+#         return qs
+
+
+class AddressCreate(CreateView):
+    template_name = "form.html"
+    form_class = AddressForm
+
+    def form_valid(self, form):
+        form.instance.customer_id = Customer.objects.get(pk=self.kwargs['customer_id'])
+        return super(AddressCreate, self).form_valid(form)
+
+
 class AddressDetail(DetailView):
     model = Address
-
-    # this is optional
-    def get_context_data(self, **kwargs):
-        context = super(AddressDetail, self).get_context_data(**kwargs)
-        print(context)
-        return context
 
 
 class AddressList(ListView):
     model = Address
 
-    # this is optional
-    def get_queryset(self, *args, **kwargs):
-        qs = super(AddressList, self).get_queryset(*args, **kwargs).order_by('-street')
-        #qs = super(AddressList, self).get_queryset(*args, **kwargs).filter(title__startswith='670')
-        print(qs)
-        print(qs.first())
-        return qs
 
-
-def index(request):
-    return HttpResponse("This is the address page")
-
-
-@login_required()
-def address_model_create_view(request, customer_id=None):
-    form = AddressForm(request.POST or None)
-    if form.is_valid():
-        obj = form.save(commit=False)
-        # make modifications here
-        cust_obj = get_object_or_404(Customer, pk=customer_id)
-        obj.customer_id = cust_obj
-        form.save()
-        messages.success(request, "Address was created successfully!")
-        return redirect('customer_app:detail', pk=cust_obj.pk)
-    return render(request, 'address/address_create.html', {'form': form})
