e0c139fe9f8972a3659b71932ef916a3a69da121
ande0581 <jeffrey.d.anderson@gmail.com>
2017-01-02 19:11:48 -0600

    timepicker working with django crispy forms

--- src/bid/forms.py
+++ src/bid/forms.py
@@ -1,14 +1,22 @@
 from django import forms
 from bid.models import Bid
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 
 class BidInitialForm(forms.ModelForm):
     description = forms.CharField(max_length=2000, widget=forms.Textarea(attrs={'rows': 3, 'cols': 20}))
     scheduled_bid_date = forms.DateTimeField()
+    notes = forms.CharField(required=True)
 
     class Meta:
         model = Bid
-        fields = ('description', 'scheduled_bid_date')
+        fields = ('description', 'scheduled_bid_date', 'notes')
+
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.add_input(Submit('login', 'Save Bid', css_class='btn=primary'))
 
 
 class BidForm(forms.ModelForm):
@@ -17,4 +25,42 @@ class BidForm(forms.ModelForm):
 
     class Meta:
         model = Bid
-        fields = ('description', 'scheduled_bid_date')
+        fields = ('description', 'notes', 'status', 'scheduled_bid_date',
+                  'tentative_start', 'actual_start', 'completion_date', 'down_payment_amount', 'down_payment_date',
+                  'final_payment_amount', 'final_payment_date')
+
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.add_input(Submit('login', 'Save Bid', css_class='btn=primary'))
+
+
+
+"""
+https://godjango.com/29-crispy-forms/
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Field
+from crispy_forms.bootstrap import (
+    PrependedText, PrependedAppendedText, FormActions)
+
+
+class CreditCardForm(forms.Form):
+    fullname = forms.CharField(label="Full Name", required=True)
+    card_number = forms.CharField(label="Card", required=True, max_length=16)
+    expire = forms.DateField(label="Expire Date", input_formats=['%m/%y'])
+    ccv = forms.IntegerField(label="ccv")
+    notes = forms.CharField(label="Order Notes", widget=forms.Textarea())
+
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.form_class = 'form-horizontal'
+    helper.label_class = 'col-sm-2'
+    helper.field_class = 'col-sm-4'
+    helper.layout = Layout(
+        Field('fullname', css_class='input-sm'),
+        Field('card_number', css_class='input-sm'),
+        Field('expire', css_class='input-sm'),
+        Field('ccv', css_class='input-sm'),
+        Field('notes', rows=3),
+        FormActions(Submit('purchase', 'purchase', css_class='btn-primary'))
+    )
+"""
