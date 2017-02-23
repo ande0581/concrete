25b01de7a6694261657f732390c34530bc59f0a7
ande0581 <jeffrey.d.anderson@gmail.com>
2017-02-22 19:29:47 -0600

    minor improvements to js script. Fixed the layout of html calculator page to render nicer

--- src/static/js/calculator.js
+++ src/static/js/calculator.js
@@ -13,13 +13,13 @@ function CalculateSlab(){
 	document.getElementById("slab-cubic-yards").innerText = (" " + cubic_yards);
 	document.getElementById("slab-80lb").innerText = (" " + bags_80);
 	document.getElementById("slab-60lb").innerText = (" " + bags_60);
-};
+}
 
 function CalculateRound(){
 	var diameter = document.getElementById('round-diameter').valueAsNumber;
 	var depth = document.getElementById('round-depth').valueAsNumber;
 	var quantity = document.getElementById('round-quantity').valueAsNumber;
-	var area = 3.14159265359 * Math.pow(((diameter / 12) / 2), 2);
+	var area = Math.PI * Math.pow(((diameter / 12) / 2), 2);
 	depth /= 12;
 
 	var cubic_yards = ((area * depth * quantity) /27).toFixed(2);
@@ -29,7 +29,7 @@ function CalculateRound(){
 	document.getElementById("round-cubic-yards").innerText = (" " + cubic_yards);
 	document.getElementById("round-80lb").innerText = (" " + bags_80);
 	document.getElementById("round-60lb").innerText = (" " + bags_60);
-};
+}
 
 function CalculateTriangle(){
 	var sideA = document.getElementById('triangle-sideA').valueAsNumber;
@@ -41,14 +41,14 @@ function CalculateTriangle(){
 	var area = Math.sqrt(perimeter * (perimeter - sideA) * (perimeter - sideB) * (perimeter - sideC));
 
 
-	var cubic_yards = ((area * thickness) / 27).toFixed(2)
-	var bags_80 = ((area * thickness) / .6).toFixed(2)
-	var bags_60 = ((area * thickness) / .45).toFixed(2)
+	var cubic_yards = ((area * thickness) / 27).toFixed(2);
+	var bags_80 = ((area * thickness) / .6).toFixed(2);
+	var bags_60 = ((area * thickness) / .45).toFixed(2);
 
 	document.getElementById("triangle-cubic-yards").innerText = (" " + cubic_yards);
 	document.getElementById("triangle-80lb").innerText = (" " + bags_80);
 	document.getElementById("triangle-60lb").innerText = (" " + bags_60);
-};
+}
 
 
 document.getElementById("slab-calculate").addEventListener('click', CalculateSlab);
