bc284c606fd1c3dc41ee7379bc75c9f8a7bb3a2b
ande0581 <jeffrey.d.anderson@gmail.com>
2017-03-01 19:37:17 -0600

    added plural name to category model

--- src/category/models.py
+++ src/category/models.py
@@ -9,6 +9,9 @@ class Category(models.Model):
     class Meta:
         ordering = ['name']
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
