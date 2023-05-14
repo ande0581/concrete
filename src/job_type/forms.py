fc9c3a12697edeb4fac4693c8bd3b9f5391d5b5b
Jeff Anderson <ande0581>
2023-05-14 13:48:31 -0500

    add check to job name to not allow a period

--- src/job_type/forms.py
+++ src/job_type/forms.py
@@ -14,3 +14,13 @@ class JobTypeForm(forms.ModelForm):
     helper = FormHelper()
     helper.form_method = 'POST'
     helper.add_input(Submit('login', 'Save Job Type', css_class='btn=primary'))
+
+    def clean(self):
+        """
+        If the category is 'Block' or 'Block-Cap' require dimensions get populated in the form
+        """
+        cleaned_data = super(JobTypeForm, self).clean()
+        description = cleaned_data.get('description')
+
+        if '.' in description:
+            raise forms.ValidationError('You cannot use a period (.) in the description')
