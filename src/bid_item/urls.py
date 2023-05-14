906285d2bcab26b0f252b5e89921887b85e307b3
Jeff Anderson <ande0581>
2023-05-14 13:36:52 -0500

    add period to url regex to allow a period in the job name field

--- src/bid_item/urls.py
+++ src/bid_item/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^create_custom/(?P<bid>\d+)/$', BidItemCustomCreate.as_view(), name='bid_item_custom_create'),
     url(r'^(?P<pk>\d+)/update/$', BidItemUpdate.as_view(), name='bid_item_update'),
     url(r'^(?P<pk>\d+)/delete/$', BidItemDelete.as_view(), name='bid_item_delete'),
-    url(r'^(?P<bid_id>\d+)/(?P<job_name>\w+)/group_delete/$', BidItemGroupDelete.as_view(),
+    url(r'^(?P<bid_id>\d+)/(?P<job_name>\w+.)/group_delete/$', BidItemGroupDelete.as_view(),
         name='bid_item_group_delete'),
 ]
